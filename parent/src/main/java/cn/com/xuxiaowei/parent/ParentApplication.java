84e807249ab1ecc8da7c0ab739a8a462b4791d6e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-22 20:49:40 +0800

    add parent

---
+++ parent/src/main/java/cn/com/xuxiaowei/parent/ParentApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentApplication.class, args);
+    }
+
+}
